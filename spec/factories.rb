8a5210eb601c4b24339de356b71d6b0447798130
Ryan Troxler <ryan@kicksite.net>
2022-02-19 10:29:45 -0600

    Allow `sign_in create(:user)`

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,12 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
     first_name { Faker::Name.first_name }
-    first_name { Faker::Name.last_name }
+    last_name { Faker::Name.last_name }
+
+    firm
+  end
+
+  factory(:firm) do
+    name { Faker::Name.name }
   end
 end
